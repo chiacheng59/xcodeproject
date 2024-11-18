1dabe122f7e3e97c19a83ad16e345e6a21378c52
peter li <peter870509@gmail.com>
2024-11-18 09:08:09 +0800

    Initial Commit

---
+++ project01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project01
+//
+//  Created by 李家誠 on 2024/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
